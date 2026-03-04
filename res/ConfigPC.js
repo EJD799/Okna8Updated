ad9e26851dcc75cc5a93593b6f1bf57a8c5dd999
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-03-04 17:50:59 +0900

    Add files via upload

---
+++ res/ConfigPC.js
@@ -0,0 +1,12 @@
+var ConfigPC = {
+    CPU: 'Intel(R) Core(TM) CPU i7-2677M @ 1.80GHz',
+    CPUFreq: 1800,
+    RAM: 8192,
+    GPU: 'NVIDIA GeForce GTX 1660',
+    OEM: {
+        Manufacturer: 'Sony',
+        Model: 'VAIO SVP132A1CV',
+        Website: null,
+        PhoneNumber: null,
+    }
+}
---
+++ res/ver.js
@@ -0,0 +1,11 @@
+﻿const VERSION = {
+    build: 110,
+    ver: '1.0.6',
+    date: '20240318-0715',
+    locales: [
+        ['ru-ru', 'Русский'],
+        ['en-us', 'English (United States)'],
+    ],
+    prerelease: false,
+    server: 'https://igorpc.ru'
+}
